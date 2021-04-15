687ceeab42540a4ed3a5440c3826b9ccacc328c4
Rafal <rafal.opial@coderslab.pl>
2021-04-15 19:49:28 +0200

    Add another assertion for search product functionality

--- src/test/java/pl/coderslab/pageobjectpatternexample/cucumber/SearchProductsSteps.java
+++ src/test/java/pl/coderslab/pageobjectpatternexample/cucumber/SearchProductsSteps.java
@@ -1,5 +1,6 @@
 package pl.coderslab.pageobjectpatternexample.cucumber;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -10,13 +11,14 @@ import pl.coderslab.pageobjectpatternexample.pageobject.MyStorePage;
 import java.util.List;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
 
 public class SearchProductsSteps {
     private WebDriver driver;
     private MyStorePage myStorePage;
 
     @Given("^Landing page displayed$")
-    public void ddd() {
+    public void landingPageDisplayed() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
         this.driver = new ChromeDriver();
         this.driver.get("https://prod-kurs.coderslab.pl/index.php");
@@ -32,5 +34,17 @@ public class SearchProductsSteps {
     public void atLeastOneProductFound() {
         List<String> displayedProductsNames = this.myStorePage.getDisplayedProductsNames();
         assertNotEquals(0, displayedProductsNames.size());
+    }
+
+    @And("^All found products contain word (.*)$")
+    public void allFoundProductsContainWordMug(String searchQuery) {
+        List<String> displayedProductsNames = this.myStorePage.getDisplayedProductsNames();
+        for (int i=0; i<displayedProductsNames.size(); i++){
+            if(!displayedProductsNames.get(i).contains(searchQuery)){
+                fail("Returned product that does not contain search query in name. " +
+                        "Search query: "+searchQuery+
+                        " Failing product name: "+displayedProductsNames.get(i));
+            }
+        }
     }
 }
--- src/test/resources/cucumber/features/search-products.feature
+++ src/test/resources/cucumber/features/search-products.feature
@@ -3,3 +3,4 @@ Feature: Search Products
     Given Landing page displayed
     When Search catalog for Mug
     Then At least one product found
+    And All found products contain word Mug
