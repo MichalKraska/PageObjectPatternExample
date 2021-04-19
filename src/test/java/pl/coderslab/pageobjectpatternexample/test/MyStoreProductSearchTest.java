518d8e0a6f153ceb9e9b60441071f2cd77962306
Rafal <rafal.opial@coderslab.pl>
2021-04-19 21:33:46 +0200

    screenshot example

--- src/test/java/pl/coderslab/pageobjectpatternexample/test/MyStoreProductSearchTest.java
+++ src/test/java/pl/coderslab/pageobjectpatternexample/test/MyStoreProductSearchTest.java
@@ -2,10 +2,16 @@ package pl.coderslab.pageobjectpatternexample.test;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pl.coderslab.pageobjectpatternexample.pageobject.MyStorePage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.Assert.assertNotEquals;
@@ -23,8 +29,14 @@ public class MyStoreProductSearchTest {
     }
 
     @Test
-    public void shouldSearchOnlyProductsContainingSearchQuery(){
+    public void shouldSearchOnlyProductsContainingSearchQuery() throws IOException {
         this.myStorePage.searchCatalog("Mug");
+        TakesScreenshot screenshot = (TakesScreenshot)driver;
+//Saving the screenshot in desired location
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+//Path to the location to save screenshot
+        Files.copy(source.toPath(), Paths.get("path", "to", "your", "Screenshot.png"));
+
         List<String> displayedProductsNames = this.myStorePage.getDisplayedProductsNames();
         assertNotEquals(0, displayedProductsNames.size());
     }
